67d8cacabe0fd72f81dbd76a97dbabc4150372c0
Max Zhou <zhouziheng02@gmail.com>
2024-08-14 10:44:37 -0400

    Module 1 assignment submission updated

--- JSIntroUM_module1_assignment/js/gallery.js
+++ JSIntroUM_module1_assignment/js/gallery.js
@@ -3,7 +3,8 @@
 function upDate(previewPic){
  /* In this function you should 
     1) change the url for the background image of the div with the id = "image" 
-    to the source file of the preview image*/
+    to the source file of the preview image
+    */
     document.getElementById("image").style.backgroundImage = 'url("' + previewPic.getAttribute("src") + '")';
 /*  2) Change the text  of the div with the id = "image" 
     to the alt text of the preview image 
@@ -15,8 +16,9 @@ function upDate(previewPic){
 	function unDo(){
      /* In this function you should 
     1) Update the url for the background image of the div with the id = "image" 
-    back to the orginal-image.  You can use the css code to see what that original URL was*/
-    document.getElementById("image").style.backgroundImage = "none";
+    back to the orginal-image.  You can use the css code to see what that original URL was
+    */
+    document.getElementById("image").style.backgroundImage = "url('')";
 /*  2) Change the text  of the div with the id = "image" 
     back to the original text.  You can use the html code to see what that original text was
     */
